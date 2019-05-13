6de33aeebeb671b0cb80c701a793ddb052224df3
Herivelto Junior <herijr@gmail.com>
2019-05-13 18:12:21 -0300

    shmup - sprite do jogador e controles

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import pygame
 import random
 import os
 
-# tamanho da janela e freamerate
-WIDTH = 800
+# tamanho da janela e framerate
+WIDTH = 480
 HEIGHT = 600
-FPS = 30
+FPS = 60
 
 # define colors
 
@@ -20,33 +20,48 @@ BLUE = (0, 0, 255)
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
 
-
 class Player(pygame.sprite.Sprite):
     # sprite for the Player
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert()
-        self.image.set_colorkey(BLACK)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        self.y_speed = 5
+        self.rect.centerx = (WIDTH / 2)
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+        self.speedy = 0
 
     def update(self):
-        self.rect.x += 5
-        self.rect.y += self.y_speed
-        if self.rect.bottom > HEIGHT - 200:
-            self.y_speed = -5
-        if self.rect.top < 200:
-            self.y_speed = 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
+        self.speedx = 0
+        self.speedy = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        if keystate[pygame.K_UP]:
+            self.speedy = -8
+        if keystate[pygame.K_DOWN]:
+            self.speedy = 8
+
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
 
 # inicia pygame e cria janela
 pygame.init()
 pygame.mixer.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
@@ -66,14 +81,9 @@ while running:
     # Update
     all_sprites.update()
     # Draw / render
-    screen.fill(YELLOW)
+    screen.fill(BLACK)
     all_sprites.draw(screen)
     # *after* drawing everythind, flip the display
     pygame.display.flip()
 
 pygame.quit()
-
-
-
-
-
