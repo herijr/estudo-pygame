10677ec7d8a2157c0e59071836d03671a7dbf18d
Herivelto Junior <herijr@gmail.com>
2019-05-13 18:00:33 -0300

    usando sprites

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 800
+HEIGHT = 600
 FPS = 30
 
 # define colors
@@ -12,14 +12,35 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+class Player(pygame.sprite.Sprite):
+    # sprite for the Player
+    def __init__(self):
+        # this line is required to properly create the sprite
+        pygame.sprite.Sprite.__init__(self)
+        # create a plain rectangle for the sprite image
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        # find the rectangle that encloses the image
+        self.rect = self.image.get_rect()
+        # center the sprite on the screen
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        # any code here will happen every time the game loop updates
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Sprite Example")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 # Game loop
 running = True
 while running:
