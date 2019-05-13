703a829a3f550260e6d6d6a67c0b6973d82bd5f0
Herivelto Junior <herijr@gmail.com>
2019-05-13 18:09:32 -0300

    usando sprites 02

--- main.py
+++ main.py
@@ -1,64 +1,79 @@
 import pygame
 import random
+import os
 
+# tamanho da janela e freamerate
 WIDTH = 800
 HEIGHT = 600
 FPS = 30
 
 # define colors
+
 WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# set up assets folders
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+
 class Player(pygame.sprite.Sprite):
     # sprite for the Player
     def __init__(self):
-        # this line is required to properly create the sprite
         pygame.sprite.Sprite.__init__(self)
-        # create a plain rectangle for the sprite image
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(GREEN)
-        # find the rectangle that encloses the image
+        self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert()
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        # center the sprite on the screen
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.y_speed = 5
 
     def update(self):
-        # any code here will happen every time the game loop updates
         self.rect.x += 5
+        self.rect.y += self.y_speed
+        if self.rect.bottom > HEIGHT - 200:
+            self.y_speed = -5
+        if self.rect.top < 200:
+            self.y_speed = 5
         if self.rect.left > WIDTH:
             self.rect.right = 0
 
-# initialize pygame and create window
+# inicia pygame e cria janela
 pygame.init()
 pygame.mixer.init()
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Sprite Example")
+pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+
 # Game loop
-running = True
+running =True
 while running:
-    # keep loop running at the right speed
+    # keep loop running ar the right speed
     clock.tick(FPS)
-    # Process input (events)
+    # Procees input(events)
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
-
     # Update
     all_sprites.update()
-
     # Draw / render
-    screen.fill(BLACK)
+    screen.fill(YELLOW)
     all_sprites.draw(screen)
-    # *after* drawing everything, flip the display
+    # *after* drawing everythind, flip the display
     pygame.display.flip()
 
 pygame.quit()
+
+
+
+
+
